85d7d081ca10b590f9282eb37d1fa8eb76608677
giyushino <allanzhang440@gmail.com>
2024-07-14 20:26:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -84,6 +84,28 @@ def overlayImages(img1, img2, w1, w2, gamma = 0):
     if cv2.waitKey(0) & 0xff == 27:
         cv2.destroyAllWindows()
 
+def detectLines(img):
+    img = cv2.imread("{0}".format(img))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 120)
+
+    for r_theta in lines:
+        arr = np.array(r_theta[0], dtype = np.float64)
+        r, theta = arr
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * r
+        y0 = b * r
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imwrite('linesDetected.jpg', img)
+    openImage("linesDetected.jpg")
+
 
 openImage("coca-cola-logo.png")
 
