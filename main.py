49d1b9625bf864af90b27dedafe2a163e93de2a1
giyushino <allanzhang440@gmail.com>
2024-07-14 17:07:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#import cv2
-#import numpy as np
-#import os
-#import matplotlib
-#from matplotlib import pyplot as plt
-#from zipfile import ZipFile
-#from urllib.request import urlretrieve
-#from IPython.display import Image
+import cv2
+import numpy as np
+import os
+import matplotlib
+from matplotlib import pyplot as plt
+from zipfile import ZipFile
+from urllib.request import urlretrieve
+from IPython.display import Image
 
 def download_and_unzip(url, save_path):
     print(f"Downloading and extracting assests....", end="")
