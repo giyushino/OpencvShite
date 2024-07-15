8f57859a98446113861ea69d3d0e6d984a60efec
giyushino <allanzhang440@gmail.com>
2024-07-14 17:06:29 -0700

    ain.py

---
+++ main.py
@@ -0,0 +1,40 @@
+#import cv2
+#import numpy as np
+#import os
+#import matplotlib
+#from matplotlib import pyplot as plt
+#from zipfile import ZipFile
+#from urllib.request import urlretrieve
+#from IPython.display import Image
+
+def download_and_unzip(url, save_path):
+    print(f"Downloading and extracting assests....", end="")
+
+    # Downloading zip file using urllib package.
+    urlretrieve(url, save_path)
+
+    try:
+        # Extracting zip file using the zipfile package.
+        with ZipFile(save_path) as z:
+            # Extract ZIP file contents in the same directory.
+            z.extractall(os.path.split(save_path)[0])
+
+        print("Done")
+
+    except Exception as e:
+        print("\nInvalid file.", e)
+
+# Download if assest ZIP does not exists.
+if not os.path.exists(asset_zip_path):
+    download_and_unzip(URL, asset_zip_path)
+
+# Open Videos
+capture = cv2.VideoCapture("lp_image.mov")
+while True:
+  isTrue, frame = capture.read()
+  cv2.imshow('Video', frame)
+  if cv2.waitKey(20) & 0xFF==ord('d'):
+    break
+capture.release()
+cv2.destroyAllWindows()
+
