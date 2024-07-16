c45ff46785a9370aba29ebe4f75dfddbe2107df0
giyushino <allanzhang440@gmail.com>
2024-07-15 17:41:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -217,4 +217,26 @@ def vidEdgeCapture(vid):
     cap.release()
     cv2.destroyAllWindows()
 
+def captureFrames(vid):
+    cam = cv2.VideoCapture(vid)
+    try:
+        if not os.path.exists("data"):
+            os.makedirs("data")
+    except OSError:
+        print("Error. Could not create directory of data")
+
+    current_frame = 0
+    while True:
+        ret, frame = cam.read()
+        if ret:
+            name = './data/frame' + str(current_frame) + '.jpg'
+            print ('Creating...' + name)
+            cv2.imwrite(name, frame)
+            current_frame += 1
+        else:
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
 
