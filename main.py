8ce352a94d028a0500e8b28df009248f8b66d171
giyushino <allanzhang440@gmail.com>
2024-07-24 16:53:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -334,4 +334,50 @@ def betterEdge(vid):
     cap.release()
 
 
+# Open terminal; type python main.py(whatever the file name you're writing on is called) --image file_name 
+def click(event, x, y, flags, param):
+    global refPt, cropping
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x, y)]
+        cropping = True
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        refPt.append((x, y))
+        cropping = False
+
+    cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+    cv2.imshow("image", image)
+
+    if cv2.waitKey(0) & 0xFF == ord('d'):
+        cv2.destroyAllWindows()
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to Image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click)
+
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("image", image)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		image = clone.copy()
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(refPt) == 2:
+	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+	cv2.imshow("ROI", roi)
+	cv2.waitKey(0)
+# close all open windows
+cv2.destroyAllWindows()
+
 
